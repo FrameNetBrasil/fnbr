ee87a37ea3c367cb55eaf37bd781f789279cf4dd
elymatos <ely.matos@gmail.com>
2017-10-06 10:39:46 -0300

    SQL scripts - maintenance and loads

---
+++ docs/database/add_entry_layertype.sql
@@ -0,0 +1,78 @@
+insert into entry (entry, name, description, nick, idLanguage) 
+select entry.entry, entry.name, entry.description, entry.nick, 4
+from entry join layertype on (entry.entry = layertype.entry) 
+where (entry.idlanguage=2)
+and (entry.entry not in ('lty_c','lty_scon'));
+
+insert into entry (entry, name, description, nick, idLanguage) 
+select entry.entry, entry.name, entry.description, entry.nick, 5
+from entry join layertype on (entry.entry = layertype.entry) 
+where (entry.idlanguage=2)
+and (entry.entry not in ('lty_c','lty_scon'));
+
+insert into entry (entry, name, description, nick, idLanguage) 
+select entry.entry, entry.name, entry.description, entry.nick, 6
+from entry join layertype on (entry.entry = layertype.entry) 
+where (entry.idlanguage=2)
+and (entry.entry not in ('lty_c','lty_scon'));
+
+insert into entry (entry, name, description, nick, idLanguage) 
+select entry.entry, entry.name, entry.description, entry.nick, 7
+from entry join layertype on (entry.entry = layertype.entry) 
+where (entry.idlanguage=2)
+and (entry.entry not in ('lty_c','lty_scon'));
+
+insert into entry (entry, name, description, nick, idLanguage) 
+select entry.entry, entry.name, entry.description, entry.nick, 8
+from entry join layertype on (entry.entry = layertype.entry) 
+where (entry.idlanguage=2)
+and (entry.entry not in ('lty_c','lty_scon'));
+
+insert into entry (entry, name, description, nick, idLanguage) 
+select entry.entry, entry.name, entry.description, entry.nick, 9
+from entry join layertype on (entry.entry = layertype.entry) 
+where (entry.idlanguage=2)
+and (entry.entry not in ('lty_c','lty_scon'));
+
+insert into entry (entry, name, description, nick, idLanguage) 
+select entry.entry, entry.name, entry.description, entry.nick, 10
+from entry join layertype on (entry.entry = layertype.entry) 
+where (entry.idlanguage=2)
+and (entry.entry not in ('lty_c','lty_scon'));
+
+insert into entry (entry, name, description, nick, idLanguage) 
+select entry.entry, entry.name, entry.description, entry.nick, 11
+from entry join layertype on (entry.entry = layertype.entry) 
+where (entry.idlanguage=2)
+and (entry.entry not in ('lty_c','lty_scon'));
+
+insert into entry (entry, name, description, nick, idLanguage) 
+select entry.entry, entry.name, entry.description, entry.nick, 12
+from entry join layertype on (entry.entry = layertype.entry) 
+where (entry.idlanguage=2)
+and (entry.entry not in ('lty_c','lty_scon'));
+
+insert into entry (entry, name, description, nick, idLanguage) 
+select entry.entry, entry.name, entry.description, entry.nick, 13
+from entry join layertype on (entry.entry = layertype.entry) 
+where (entry.idlanguage=2)
+and (entry.entry not in ('lty_c','lty_scon'));
+
+insert into entry (entry, name, description, nick, idLanguage) 
+select entry.entry, entry.name, entry.description, entry.nick, 14
+from entry join layertype on (entry.entry = layertype.entry) 
+where (entry.idlanguage=2)
+and (entry.entry not in ('lty_c','lty_scon'));
+
+insert into entry (entry, name, description, nick, idLanguage) 
+select entry.entry, entry.name, entry.description, entry.nick, 15
+from entry join layertype on (entry.entry = layertype.entry) 
+where (entry.idlanguage=2)
+and (entry.entry not in ('lty_c','lty_scon'));
+
+insert into entry (entry, name, description, nick, idLanguage) 
+select entry.entry, entry.name, entry.description, entry.nick, 16
+from entry join layertype on (entry.entry = layertype.entry) 
+where (entry.idlanguage=2)
+and (entry.entry not in ('lty_c','lty_scon'));
+
