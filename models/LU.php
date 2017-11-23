3fe30c29fdf472f09abcd52c230a55863c33f77c
elymatos <ely.matos@gmail.com>
2017-11-23 15:46:43 -0200

    Correction on getting LU SemanticType

--- models/LU.php
+++ models/LU.php
@@ -56,6 +56,7 @@ class LU extends map\LUMap
         $data = parent::getData();
         $data->idFrame = $this->idFrame;
         $criteria = Base::relationCriteria('LU','SemanticType','rel_hastype','SemanticType.idEntity');
+        $criteria->where("LU.idEntity", "=", $this->getIdEntity());
         $idEntitySemanticType = $criteria->asQuery()->getResult()[0]['idEntity'];
         if ($idEntitySemanticType) {
             $st = new SemanticType();
